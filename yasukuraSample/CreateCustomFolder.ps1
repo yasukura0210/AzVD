0016fb1ef487c6d3f9e351b50a86ba92acab46cf
yasukura0210 <167849100+yasukura0210@users.noreply.github.com>
2025-06-27 14:31:44 +0900

    Update CreateCustomFolder.ps1

--- yasukuraSample/CreateCustomFolder.ps1
+++ yasukuraSample/CreateCustomFolder.ps1
@@ -29,7 +29,19 @@ Copy-UserInternationalSettingsToSystem -WelcomeScreen $True -NewUser $True
 # タイムゾーンを日本標準時(東京)に設定 -> Sysprepでリセットされている可能性が高い
 # Set-TimeZone -Id "Tokyo Standard Time"
 # [代替案]　C:\Windows\Setup\Scripts\SetupComplete.cmdでタイムゾーンを変更する
+
 $scriptPath = "C:\Windows\Setup\Scripts\SetupComplete.cmd"
-New-Item -Path $scriptPath -ItemType File -Force
-Add-Content -Path $scriptPath -Value '@echo off'
-Add-Content -Path $scriptPath -Value 'powershell.exe -ExecutionPolicy Bypass -Command "Set-TimeZone -Id ''Tokyo Standard Time''"'
+
+# フォルダが存在しない場合は作成
+$folderPath = Split-Path $scriptPath
+if (!(Test-Path $folderPath)) {
+    New-Item -Path $folderPath -ItemType Directory -Force
+}
+
+# ファイルが存在するか確認し、内容を追記
+$timezoneCommand = 'powershell.exe -ExecutionPolicy Bypass -Command "Set-TimeZone -Id ''Tokyo Standard Time''"'
+
+if (!(Test-Path $scriptPath)) {
+    Add-Content -Path $scriptPath -Value "@echo off"
+}
+Add-Content -Path $scriptPath -Value $timezoneCommand

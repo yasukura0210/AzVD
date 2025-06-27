0992905d31806e3bb1df37eaa29c13c29a42a7bc
yasukura0210 <167849100+yasukura0210@users.noreply.github.com>
2025-06-27 20:08:48 +0900

    Update CreateCustomFolder.ps1

--- yasukuraSample/CreateCustomFolder.ps1
+++ yasukuraSample/CreateCustomFolder.ps1
@@ -27,24 +27,45 @@ Set-WinSystemLocale -SystemLocale ja-JP
 Copy-UserInternationalSettingsToSystem -WelcomeScreen $True -NewUser $True
 
 # タイムゾーンを日本標準時(東京)に設定 -> Sysprepでリセットされている可能性が高い
-# Set-TimeZone -Id "Tokyo Standard Time"
-# [代替案]　C:\Windows\Setup\Scripts\SetupComplete.cmdでタイムゾーンを変更する
+Set-TimeZone -Id "Tokyo Standard Time"
 
-$scriptPath = "C:\Windows\Setup\Scripts\SetupComplete.cmd"
-$timezoneCommand = 'powershell.exe -ExecutionPolicy Bypass -Command "Set-TimeZone -Id ''Tokyo Standard Time''"'
+# [代替案#1]　C:\Windows\Setup\Scripts\SetupComplete.cmdでタイムゾーンを変更する -> 失敗
+#$scriptPath = "C:\Windows\Setup\Scripts\SetupComplete.cmd"
+#$timezoneCommand = 'powershell.exe -ExecutionPolicy Bypass -Command "Set-TimeZone -Id ''Tokyo Standard Time''"'
+## フォルダが存在しない場合は作成
+#$folderPath = Split-Path $scriptPath
+#if (!(Test-Path $folderPath)) {
+#    New-Item -Path $folderPath -ItemType Directory -Force
+#}
+## ファイルが存在しない場合は初期化
+#if (!(Test-Path $scriptPath)) {
+#    Set-Content -Path $scriptPath -Value "@echo off" -Encoding ASCII
+#}
+## タイムゾーン設定が未記載なら追記
+#if (-not (Get-Content $scriptPath | Select-String "Set-TimeZone")) {
+#    Add-Content -Path $scriptPath -Value "`r`n" + $timezoneCommand -Encoding ASCII
+#}
 
-# フォルダが存在しない場合は作成
-$folderPath = Split-Path $scriptPath
-if (!(Test-Path $folderPath)) {
-    New-Item -Path $folderPath -ItemType Directory -Force
-}
+# [代替案#2]　C:\Windows\Panther\Unattend\Unattend.xml で sysprep時にタイムゾーンを変更する
+$unattendContent = @"
+<?xml version="1.0" encoding="utf-8"?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+  <settings pass="oobeSystem">
+    <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64"
+               publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
+      <TimeZone>Tokyo Standard Time</TimeZone>
+      <FirstLogonCommands>
+        <SynchronousCommand wcm:action="add">
+          <Order>1</Order>
+          <Description>Set TimeZone</Description>
+          <CommandLine>powershell.exe -ExecutionPolicy Bypass -Command "Set-TimeZone -Id 'Tokyo Standard Time'"</CommandLine>
+        </SynchronousCommand>
+      </FirstLogonCommands>
+    </component>
+  </settings>
+</unattend>
+"@
 
-# ファイルが存在しない場合は初期化
-if (!(Test-Path $scriptPath)) {
-    Set-Content -Path $scriptPath -Value "@echo off" -Encoding ASCII
-}
-
-# タイムゾーン設定が未記載なら追記
-if (-not (Get-Content $scriptPath | Select-String "Set-TimeZone")) {
-    Add-Content -Path $scriptPath -Value "`r`n" + $timezoneCommand -Encoding ASCII
-}
+$unattendPath = "C:\Windows\Panther\Unattend\Unattend.xml"
+New-Item -ItemType Directory -Path (Split-Path $unattendPath) -Force
+$unattendContent | Out-File -FilePath $unattendPath -Encoding utf8

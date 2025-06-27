43c483c5efdde284a16f2ef53b33a018a61c869c
yasukura0210 <167849100+yasukura0210@users.noreply.github.com>
2025-06-28 08:53:15 +0900

    Update CreateCustomFolder.ps1

--- yasukuraSample/CreateCustomFolder.ps1
+++ yasukuraSample/CreateCustomFolder.ps1
@@ -27,7 +27,7 @@ Set-WinSystemLocale -SystemLocale ja-JP
 Copy-UserInternationalSettingsToSystem -WelcomeScreen $True -NewUser $True
 
 # タイムゾーンを日本標準時(東京)に設定 -> Sysprepでリセットされている可能性が高い
-Set-TimeZone -Id "Tokyo Standard Time"
+# Set-TimeZone -Id "Tokyo Standard Time"
 
 # [代替案#1]　C:\Windows\Setup\Scripts\SetupComplete.cmdでタイムゾーンを変更する -> 失敗
 #$scriptPath = "C:\Windows\Setup\Scripts\SetupComplete.cmd"
@@ -49,7 +49,9 @@ Set-TimeZone -Id "Tokyo Standard Time"
 # [代替案#2]　C:\Windows\Panther\Unattend\Unattend.xml で sysprep時にタイムゾーンを変更する
 $unattendContent = @"
 <?xml version="1.0" encoding="utf-8"?>
-<unattend xmlns="urn:schemas-microsoft-com:unattend">
+<unattend xmlns="urn:schemas-microsoft-com:unattend" 
+          xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" 
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <settings pass="oobeSystem">
     <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64"
                publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">

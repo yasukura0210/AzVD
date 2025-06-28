585810c70efb921a51900872ae7ece7c02504607
yasukura0210 <167849100+yasukura0210@users.noreply.github.com>
2025-06-28 13:33:37 +0900

    Update CustomScript2.ps1

--- yasukuraSample/CustomScript2.ps1
+++ yasukuraSample/CustomScript2.ps1
@@ -5,12 +5,23 @@ $folderName = "CustomFolder01"                              # 作成するフォ
 $defaultDesktop = "C:\Users\Default\Desktop"
 $targetPath   = Join-Path -Path $defaultDesktop -ChildPath $folderName
 
+$logPath = "C:\AVDSetup\log.txt"
+if (!(Test-Path -Path "C:\AVDSetup")) {
+    New-Item -ItemType Directory -Path "C:\AVDSetup" -Force | Out-Null
+}
+
 # フォルダが存在しない場合に新規作成
-if (!(Test-Path -Path $targetPath)) {
-    New-Item -ItemType Directory -Path $targetPath -Force | Out-Null
-    Write-Host "Folder '$folderName' created in Default user Desktop ($targetPath)."
-} else {
-    Write-Host "Folder '$folderName' already exists at $targetPath."
+try {
+  if (!(Test-Path -Path $targetPath)) {
+      New-Item -ItemType Directory -Path $targetPath -Force | Out-Null
+      Write-Host "Folder '$folderName' created in Default user Desktop ($targetPath)."
+      Add-Content -Path $logPath -Value "[$(Get-Date)] Folder '$folderName' created."
+  } else {
+      Write-Host "Folder '$folderName' already exists at $targetPath."
+      Add-Content -Path $logPath -Value "[$(Get-Date)] Folder already exists."    
+  }
+} catch {
+    Add-Content -Path $logPath -Value "[$(Get-Date)] Error: $_"
 }
 
 #### システム設定

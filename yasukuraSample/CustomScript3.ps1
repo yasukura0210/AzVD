3a4923e67d8891e56effc269b9ae4f96d0398ea2
yasukura0210 <167849100+yasukura0210@users.noreply.github.com>
2025-06-29 15:29:12 +0900

    Update CustomScript3.ps1

--- yasukuraSample/CustomScript3.ps1
+++ yasukuraSample/CustomScript3.ps1
@@ -1,3 +1,20 @@
+####### 以下はOS設定 #######
+#### システム設定
+# 時刻・日付などの表示形式が言語リストに追従するよう設定
+Set-WinCultureFromLanguageListOptOut -OptOut $False
+
+# デフォルトの入力方法を日本語IMEに変更
+Set-WinDefaultInputMethodOverride -InputTip "0411:00000411"
+
+# システムロケールを日本 (ja-JP) に設定
+Set-WinSystemLocale -SystemLocale ja-JP
+
+# ようこそ画面と新規ユーザーアカウントにも現在の国際設定をコピー
+Copy-UserInternationalSettingsToSystem -WelcomeScreen $True -NewUser $True
+
+# タイムゾーンを日本標準時(東京)に設定 -> Sysprepでリセットされている模様なので無意味
+# Set-TimeZone -Id "Tokyo Standard Time"
+
 ##### カスタムフォルダ作成 #####
 # C:\Users\Default\Desktop」へのフォルダ作成 : 各ユーザーのデフォルトプロファイルとしてのデスクトップ
 # C:\Users\Public\Desktop」へのフォルダ作成 : パブリックデスクトップ（ここに作成したショートカットやフォルダはそのマシン上の全ユーザーのデスクトップに表示される）
@@ -15,60 +32,29 @@ if (!(Test-Path -Path "C:\AVDSetup")) {
 }
 
 # フォルダ作成 : folderName1
-Add-Content -Path $logPath -Value "[$(Get-Date)] Folder1 creation start."
-Add-Content -Path $logPath -Value "[$(Get-Date)] targetPath is ($targetPath1)."
-Add-Content -Path $logPath -Value "[$(Get-Date)] folderName is ($folderName1)."
+Add-Content -Path $logPath -Value "[$(Get-Date)] Folder1 creation start. TargetPath is $targetPath1. FolderName is $folderName1."
 try {
   if (!(Test-Path -Path $targetPath1)) {
       New-Item -ItemType Directory -Path $targetPath1 -Force | Out-Null
-      Write-Host "Folder '$folderName1' created in Default user Desktop ($targetPath1)."
       Add-Content -Path $logPath -Value "[$(Get-Date)] Folder '$folderName1' created."
   } else {
-      Write-Host "Folder '$folderName1' already exists."
       Add-Content -Path $logPath -Value "[$(Get-Date)] Folder already exists."    
   }
 } catch {
     Add-Content -Path $logPath -Value "[$(Get-Date)] Error: $_"
 }
-Add-Content -Path $logPath -Value "[$(Get-Date)] Folder1 creation end."
-Add-Content -Path $logPath -Value "[$(Get-Date)] targetPath is ($targetPath1)."
-Add-Content -Path $logPath -Value "[$(Get-Date)] folderName is ($folderName1)."
+Add-Content -Path $logPath -Value "[$(Get-Date)] Folder1 creation end. TargetPath is $targetPath1. FolderName is $folderName1."
 
 # フォルダ作成 : folderName2
-Add-Content -Path $logPath -Value "[$(Get-Date)] Folder2 creation start."
-Add-Content -Path $logPath -Value "[$(Get-Date)] targetPath is ($targetPath2)."
-Add-Content -Path $logPath -Value "[$(Get-Date)] folderName is ($folderName2)."
-
+Add-Content -Path $logPath -Value "[$(Get-Date)] Folder2 creation start. TargetPath is $targetPath2. FolderName is $folderName2."
 try {
   if (!(Test-Path -Path $targetPath2)) {
       New-Item -ItemType Directory -Path $targetPath2 -Force | Out-Null
-      Write-Host "Folder '$folderName2' created in Default user Desktop ($targetPath2)."
       Add-Content -Path $logPath -Value "[$(Get-Date)] Folder '$folderName2' created."
   } else {
-      Write-Host "Folder '$folderName2' already exists."
       Add-Content -Path $logPath -Value "[$(Get-Date)] Folder already exists."    
   }
 } catch {
     Add-Content -Path $logPath -Value "[$(Get-Date)] Error: $_"
 }
-Add-Content -Path $logPath -Value "[$(Get-Date)] Folder2 creation start."
-Add-Content -Path $logPath -Value "[$(Get-Date)] targetPath is ($targetPath2)."
-Add-Content -Path $logPath -Value "[$(Get-Date)] folderName is ($folderName2)."
-
-
-####### 以下はOS設定 #######
-#### システム設定
-# 時刻・日付などの表示形式が言語リストに追従するよう設定
-Set-WinCultureFromLanguageListOptOut -OptOut $False
-
-# デフォルトの入力方法を日本語IMEに変更
-Set-WinDefaultInputMethodOverride -InputTip "0411:00000411"
-
-# システムロケールを日本 (ja-JP) に設定
-Set-WinSystemLocale -SystemLocale ja-JP
-
-# ようこそ画面と新規ユーザーアカウントにも現在の国際設定をコピー
-Copy-UserInternationalSettingsToSystem -WelcomeScreen $True -NewUser $True
-
-# タイムゾーンを日本標準時(東京)に設定 -> Sysprepでリセットされている模様なので無意味
-# Set-TimeZone -Id "Tokyo Standard Time"
+Add-Content -Path $logPath -Value "[$(Get-Date)] Folder2 creation end. TargetPath is ($targetPath2). FolderName is $folderName2."
